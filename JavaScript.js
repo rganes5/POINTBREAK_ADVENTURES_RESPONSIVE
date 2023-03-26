8bb85d11bf82f61753c9c5da326b800618d55557
Ganesh R <116338267+rganes5@users.noreply.github.com>
2023-03-26 13:08:42 +0530

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+document.querySelector("#bookNow").addEventListener("click",() => {
+document.querySelector(".popup").classList.add("active");
+});
+document.querySelector("#closeBottomPopUp").addEventListener("click",() => {
+document.querySelector(".popup").classList.remove("active");
+});
+
